8a0288599bf79ba704bbae1c0d22725ec87aacd0
zwpdbh <hyperion_z@outlook.com>
2018-05-08 19:49:58 +1200

    > has bug when ROWS < NUM_THREADS for multiplication

--- pthread_demo/matrix_multiplication.cpp
+++ pthread_demo/matrix_multiplication.cpp
@@ -12,45 +12,58 @@
 
 using namespace std;
 
-void *emalloc(size_t s);
-void* thr_alloc_fn(void* arg);
-void* thr_access_fn(void* arg);
-void check_mx();
-
-#define ROWS 7
-#define COLS 11
-#define NUM_THREADS 8
-#define NUM_NODES 8
+int ROWS = 12;
+int COLS = 11;
+int NUM_THREADS = 8;
+int NUM_NODES = 8;
 
 long s_in_each_node = 0;
 float** mx = nullptr;
+float* v = nullptr;
+float* w = nullptr;
 long s = 0;
 
-typedef struct Access_info_per_thread pInfo;
-struct Access_info_per_thread {
-    // hold thread_id passed from index in the loop
+
+typedef struct pthreadInfo {
     int thread_id;
-    // hold the size, for each thread
+    long from;
     long job_size;
-};
+    int row_in_mx;
+    int p;
+    pthread_t thread;
+} pInfo;
+
+void *emalloc(size_t s);
+void *remalloc(void *p, size_t s);
+void* thr_alloc_fn(void* arg);
+void* thr_access_fn(void* arg);
+void* thr_multiply_fn(void* arg);
+void check_mx();
+void check_w();
+void construct_barrier(pInfo* pthreads);
+
 
 int main(int argc, char* argv[]) {
+
     s = ROWS * COLS;
     s_in_each_node = s / NUM_NODES;
 
     mx = (float**)emalloc(sizeof(float*) * NUM_NODES);
 
-    int* tid;   // array of int, for passing index into each thread
+    pInfo* pthreads = (pInfo*) emalloc(sizeof(*pthreads) * NUM_NODES);
     pthread_t* thread; // array of pthread_t, for catching each created thread
     int err = 0;
-    void* status;
-    tid = (int*)emalloc(sizeof(*tid) * NUM_NODES);
-    thread = (pthread_t*)emalloc(sizeof(*thread) * NUM_NODES);
+
 
     /**Allocate m by n matrix with the shape NUM_NODES * some right size*/
     for (int i = 0; i < NUM_NODES; i++) {
-        tid[i] = i;
-        err = pthread_create(&thread[i], nullptr, thr_alloc_fn, (void *)&tid[i]);
+        pthreads[i].thread_id = i;
+        pthreads[i].row_in_mx = i;
+        pthreads[i].job_size = (s/NUM_NODES);
+        if (pthreads[i].thread_id == NUM_NODES - 1) {
+            pthreads[i].job_size += (s % NUM_NODES);
+        }
+        err = pthread_create(&pthreads[i].thread, NULL, thr_alloc_fn, (void *)&pthreads[i]);
         if (err != 0) {
             printf("error during thread creation, exit..\n");
             exit(-1);
@@ -58,53 +71,73 @@ int main(int argc, char* argv[]) {
     }
 
     /**manually created barrier*/
-    for (int i = 0; i < NUM_NODES; i++) {
-        err = pthread_join(thread[i], &status);
-        if (err) {
-            printf("error, return code from pthread_join() is %d\n", *(int *)status);
-        }
-    }
-
+    construct_barrier(pthreads);
     check_mx();
-    free(tid);
 
-    pInfo* threadInfo;
-    // for catching each created threads
-    free(thread);
-    thread = (pthread_t*) emalloc(sizeof(*thread) * NUM_THREADS);
-    // for passing useful partition info into each thread
-    threadInfo = (pInfo*) emalloc(sizeof(*threadInfo) * NUM_THREADS);
-
+    pthreads =(pInfo*)remalloc(pthreads, sizeof(*pthreads) * NUM_THREADS);
     for (int i = 0; i < NUM_THREADS; i++) {
-        threadInfo[i].thread_id = i;
-        threadInfo[i].job_size = s / NUM_THREADS;
-        err = pthread_create(&thread[i], nullptr, thr_access_fn, (void *)&threadInfo[i]);
+        pthreads[i].thread_id = i;
+        pthreads[i].row_in_mx = i / (NUM_THREADS / NUM_NODES);
+        pthreads[i].p = i  - pthreads[i].row_in_mx * (NUM_THREADS / NUM_NODES);
+        pthreads[i].job_size = s / NUM_THREADS;
+        pthreads[i].from = pthreads[i].p * pthreads[i].job_size;
+        if ((pthreads[i].p + 1) % (NUM_THREADS / NUM_NODES) == 0) {
+            pthreads[i].job_size += ((s / NUM_NODES) % (NUM_THREADS / NUM_NODES));
+        }
+        if (pthreads[i].thread_id == NUM_THREADS - 1) {
+            pthreads[i].job_size +=  (s % NUM_NODES);
+        }
+
+        err = pthread_create(&pthreads[i].thread, NULL, thr_access_fn, (void *)&pthreads[i]);
         if (err != 0) {
             printf("error during thread creation, exit...\n");
             exit(-1);
         }
     }
 
-    // make sure all thread finished the execution
-    for (int i = 0; i < NUM_THREADS; i++) {
-        err = pthread_join(thread[i], &status);
-        if (err) {
-            printf("error, return code from pthread_join() is %d\n", *(int*)status);
-        }
-    }
-
+    construct_barrier(pthreads);
     printf("\n");
     check_mx();
 
+    /**do the multiplication with a vector*/
+    v = (float*)emalloc(sizeof(float) * COLS);
+    w = (float*)emalloc(sizeof(float) * ROWS);
+    for (int i = 0; i < COLS; i++) {
+        v[i] = 1;
+    }
+    for (int i = 0; i < ROWS; i++) {
+        w[i] = 0;
+    }
 
+    // distribute multiplication task on different threads
+    for (int i = 0; i < NUM_THREADS; i++) {
+        if (ROWS / NUM_THREADS == 0) {
+            pthreads[i].from = i * 1;
+            pthreads[i].job_size = 1;
+        } else {
+            pthreads[i].from  = i * (ROWS / NUM_THREADS);
+            pthreads[i].job_size = ROWS / NUM_THREADS;
+        }
+
+        if (i == NUM_THREADS - 1) {
+            pthreads[i].job_size += (ROWS % NUM_THREADS);
+        }
+        err = pthread_create(&pthreads[i].thread, NULL, thr_multiply_fn, (void*)&pthreads[i]);
+        if (err != 0) {
+            printf("error during thread creation, exit...\n");
+            exit(-1);
+        }
+    }
+
+    /**build barrier*/
+    construct_barrier(pthreads);
+    check_w();
 
     for (int i = 0; i < NUM_NODES; i++) {
         free(mx[i]);
     }
     free(mx);
-    free(threadInfo);
-    free(thread);
-
+    free(pthreads);
     exit(0);
 }
 
@@ -117,60 +150,48 @@ void *emalloc(size_t s) {
     return result;
 }
 
+void *remalloc(void *p, size_t s) {
+    void *result = realloc(p, s);
+    if (result == NULL) {
+        fprintf(stderr, "memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+    return result;
+}
+
 // thread function to use libnuma to allocate memory on specific node
 void* thr_alloc_fn(void* arg) {
-    pid_t pid;
-    pthread_t tid;
-    int i;
-
-    pid = getpid();
-    tid = pthread_self();
-    i = *(int *)arg;
-
-    printf("pid = %lu, tid = %lu, for i = %d\n", (unsigned long)pid, (unsigned long)tid, i);
-    mx[i] = (float*)emalloc(s_in_each_node * sizeof(float));
-
-    long size_for_each_node = s_in_each_node;
-    if (i == NUM_NODES -1) {
-        size_for_each_node += s % NUM_NODES;
-        printf("for thread = %d, it gets extra job, size_for_each_node = %ld\n", i, size_for_each_node);
+    pInfo info = *(pInfo*)arg;
+    *(mx + info.row_in_mx) = (float *)emalloc(info.job_size * sizeof(float));
+    for (int k = 0; k < info.job_size; k++) {
+        *(*(mx +info.row_in_mx) +k) = 0;
     }
-    for (int k = 0; k < size_for_each_node; k++) {
-        *(*(mx + i) + k) = 0;
-    }
-
-    return 0;
+    return (void *)0;
 }
 
 
 void* thr_access_fn(void* arg) {
-    pInfo info;
-    info = *(struct Access_info_per_thread*)arg;
-
-    /**node_index tells which node the current thread is executing in
-     * It depends on how many threads one node could hold on*/
-    int node_index = info.thread_id / (NUM_THREADS / NUM_NODES);
-
-    /**Only know which node this thread is executing on is not enough,
-     * since there are multiple thread working on the same node section.
-     * So we need to know which part of the section in the node is the current thread operate on*/
-    int p = info.thread_id - node_index * (NUM_THREADS / NUM_NODES);
-
-    long from = p * info.job_size;
-    long to = from + info.job_size;
-
-    // if it is the last thread in a NUMA node section
-    if ((p + 1) % (NUM_THREADS / NUM_NODES) == 0) {
-        to += s_in_each_node % (NUM_THREADS / NUM_NODES);
+    pInfo info = *(pInfo *)arg;
+    for (long i = info.from; i < info.from + info.job_size; i++) {
+        *(*(mx + info.row_in_mx) + i) = 3.0;
     }
+    return (void *)0;
+}
 
-    // if it is the last thread
-    if (info.thread_id == NUM_THREADS - 1) {
-        to = to + s % NUM_NODES;
-    }
-
-    for (long i = from; i < to; i++) {
-        *(*(mx + node_index) + i) = 3.0;
+void* thr_multiply_fn(void* arg) {
+//    eachInfo info = *(eachInfo*)arg;
+    pInfo info = *(pInfo*)arg;
+    for (int r = info.from; r < info.from  + info.job_size; ++r) {
+        // get corresponding mx(i, j)
+        for (int i = 0; i < COLS; i++) {
+            long l = r * COLS + i;
+            long row_in_mx = l / s_in_each_node - 1;
+            if (row_in_mx < 0) {
+                row_in_mx = 0;
+            }
+            long col_in_mx = l % (s / NUM_NODES);
+            w[r] += (*(*(mx + row_in_mx) + col_in_mx) * v[i]);
+        }
     }
 
     return (void *)0;
@@ -188,5 +209,24 @@ void check_mx() {
             printf("%.f ", *(*(mx + i) + j));
         }
         printf("\n");
+    }
+}
+
+void check_w() {
+    printf("w = \n");
+    for (long i = 0; i < ROWS; i++) {
+        printf("%.f ", w[i]);
+    }
+    printf("\n");
+}
+
+void construct_barrier(pInfo* pthreads) {
+    int err;
+    void* status;
+    for (int i = 0; i < NUM_THREADS; i++) {
+        err = pthread_join(pthreads[i].thread, &status);
+        if (err) {
+            printf("error, return code from pthread_join() is %d\n", *(int*)status);
+        }
     }
 }
